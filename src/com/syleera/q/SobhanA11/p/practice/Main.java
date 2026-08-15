3360c893737860c4350c5ee38efc8b2b24f7bdc3
SobhanA11 <mr.sobhan.ahmadi@gmail.com>
2026-08-15 21:31:03 +0330

    cleaned repo for next project

--- src/com/syleera/q/SobhanA11/p/practice/Main.java
+++ src/com/syleera/q/SobhanA11/p/practice/Main.java
@@ -6,25 +6,3 @@ package com.syleera.q.SobhanA11.p.practice;
  * @author SobhanA11
  */
 
-class Main {
-    public static void main(String[] args) {
-        DataBase db = new DataBase();
-        Main.printText(db.getName(), db.getLastName(), db.getNickName(), db.getCountry(), db.getCity(), db.getFavoriteCharacter(), db.getFavoriteGame(), db.getFavoriteLanguage(), db.getAge(), db.getHeaderText(), db.getBetweenText());
-    }
-
-    public static void printText(String name, String lastName, String nickName, String country, String city, String favoriteCharacter, String favoriteGame, String favoriteLanguage, int age, String headerText, String footerText) {
-        System.out.printf(
-                "%s" +
-                "\nname: %s" +
-                "\nlast name: %s" +
-                "\nnick name: %s" +
-                "\ncountry: %s" +
-                "\ncity: %s" +
-                "\nfavorite character: %s" +
-                "\nfavorite game: %s" +
-                "\nfavorite language: %s" +
-                "\nage: %s" +
-                "\n%s"
-                , headerText, name, lastName, nickName, country, city, favoriteCharacter, favoriteGame, favoriteLanguage, age, footerText);
-    }
-}
