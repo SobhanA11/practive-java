8d0434430f41133f7e6902f7745e323fc972ff7a
SobhanA11 <mr.sobhan.ahmadi@gmail.com>
2026-08-16 15:31:08 +0330

    cleaned repo for next project

--- src/com/syleera/q/SobhanA11/p/practice/Main.java
+++ src/com/syleera/q/SobhanA11/p/practice/Main.java
@@ -1,38 +1,7 @@
 package com.syleera.q.SobhanA11.p.practice;
 
-import com.syleera.q.SobhanA11.p.practice.actions.Jame;
-import com.syleera.q.SobhanA11.p.practice.actions.Tafrig;
-import com.syleera.q.SobhanA11.p.practice.actions.Tagsim;
-import com.syleera.q.SobhanA11.p.practice.actions.Zarb;
-
 /**
  * Created at 8/15/2026
  *
  * @author SobhanA11
  */
-
-public class Main {
-    Jame jame = new Jame();
-    Tafrig tafrig = new Tafrig();
-    Zarb zarb = new Zarb();
-    Tagsim tagsim = new Tagsim();
-
-    public static void main(String[] ags) {
-        Main main = new Main();
-        main.resultPrinter(DataBase.getFirstNumber(), DataBase.getSecondNumber());
-    }
-
-    public void resultPrinter(int firstNumber, int secondNumber) {
-        System.out.printf("Number 1: %s\nNumber 2: %s", firstNumber, secondNumber);
-        System.out.println("\n");
-        System.out.printf(
-                "Addition: %s\n" +
-                        "Subtraction: %s\n" +
-                        "Multiplication: %s\n" +
-                        "Division: %s",
-                jame.doJame(firstNumber, secondNumber),
-                tafrig.doTafrig(firstNumber, secondNumber),
-                zarb.doZarb(firstNumber, secondNumber),
-                tagsim.doTagsim(firstNumber, secondNumber));
-    }
-}
