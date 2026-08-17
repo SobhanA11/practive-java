29a8fb3807d06af757ea80de84cfb4a80ffb6c54
SobhanA11 <mr.sobhan.ahmadi@gmail.com>
2026-08-17 16:56:54 +0330

    added more variables on DataBase.java and make getter for them

--- src/com/syleera/q/SobhanA11/p/practice/DataBase.java
+++ src/com/syleera/q/SobhanA11/p/practice/DataBase.java
@@ -7,11 +7,114 @@ package com.syleera.q.SobhanA11.p.practice;
  */
 
 public class DataBase {
-    private String header = "====================\n       Java Number Analyzer\n====================";
+    private String equal = "====================";
+    private String welcomeHeader = equal + "\n       Java Number Analyzer\n" + equal;
     private String welcomeText = "Hello and welcome to the Java Number Analyzer!";
-    private String menu = "======================\n       MENU\n======================\n\n\"-1\": Analyze a set\n\"-3\": help\"-2\": Exit\n\nChoose: ";
+    private String menuHeader = equal + "\n       MENU\n" + equal + "\n\n\"-1\": Analyze a set\n\"-3\": help\"-2\": Exit\n\nChoose: ";
     private String numCount = "How many numbers do you want to enter?";
-    private String enterNum = "Enter number %s: ";
-    private String result = "======================\n       RESULT\n======================";
+    private String enterNumText = "Enter number %s: ";
+    private String resultHeader = equal + "\n       RESULT\n" + equal;
+    private String numberText = "Number %s: %s";
+    private String statusText = "Status: %s";
+    private String typeText = "Type: %s";
+    private String status = "Status: %s";
+    private String divisibleByThreeText = "Divisible by 3: %s";
+    private String divisibleByFiveText = "Divisible bt 5: %s";
+    private String sumText = "Sum: %s";
+    private String averageText = "Average: %s";
+    private String positivesText = "Positive numbers: %s";
+    private String negativesText = "Negative numbers: %s";
+    private String zerosText = "Zero numbers: %s";
+    private String evensText = "Even numbers: %s";
+    private String oddsText = "Odd numbers: %s";
+    private String divisibleByThreesText = divisibleByThreeText;
+    private String divisibleByFivesText = divisibleByFiveText;
 
+    public String getTypeText() {
+        return typeText;
+    }
+
+    public String getEqual() {
+        return equal;
+    }
+
+    public String getWelcomeText() {
+        return welcomeText;
+    }
+
+    public String getNumCount() {
+        return numCount;
+    }
+
+    public String getWelcomeHeader() {
+        return welcomeHeader;
+    }
+
+    public String getMenuHeader() {
+        return menuHeader;
+    }
+
+    public String getEnterNumText() {
+        return enterNumText;
+    }
+
+    public String getResultHeader() {
+        return resultHeader;
+    }
+
+    public String getNumberText() {
+        return numberText;
+    }
+
+    public String getStatusText() {
+        return statusText;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getDivisibleByThreeText() {
+        return divisibleByThreeText;
+    }
+
+    public String getDivisibleByFiveText() {
+        return divisibleByFiveText;
+    }
+
+    public String getSumText() {
+        return sumText;
+    }
+
+    public String getPositivesText() {
+        return positivesText;
+    }
+
+    public String getNegativesText() {
+        return negativesText;
+    }
+
+    public String getDivisibleByFivesText() {
+        return divisibleByFivesText;
+    }
+
+    public String getOddsText() {
+        return oddsText;
+    }
+
+    public String getDivisibleByThreesText() {
+        return divisibleByThreesText;
+    }
+
+    public String getEvensText() {
+        return evensText;
+    }
+
+    public String getZerosText() {
+        return zerosText;
+    }
+
+    public String getAverageText() {
+        return averageText;
+    }
 }
